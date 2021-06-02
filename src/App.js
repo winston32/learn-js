865dc42abe2eccb4122776e9be8bfb58155f6af8
wnanda3226 <winston.nanda@gmail.com>
2021-06-02 18:00:43 +0530

    build(root): added bootstrap,router and node saas dependencies.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import { Button } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Test() {
+  return (<div>router works</div>);
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Link to="/test"><Button variant="danger" className="m-5">Test</Button></Link>
+    <Switch>
+      <Route path="/test"><Test/></Route>
+    </Switch>
+    </Router>
   );
 }
 
